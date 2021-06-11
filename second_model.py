790428ba977a0525d3846498cce1c9a9f2fcf5d2
Simone Flavio <smoxy@users.noreply.github.com>
2021-06-11 09:41:57 +0200

    Update second_model.py

--- second_model.py
+++ second_model.py
@@ -1,26 +1,18 @@
 import os
-import datetime
-import threading
-import time
-import urllib.request
-import urllib.error
-from statistics import mean
-
 import numpy as np
 import pandas as pd
 import folium
-import googlemaps
-import polyline
-from rich.console import Console
+import time
+import urllib.request
+import urllib.error
 from folium import plugins
+from distance import calculate_distance
 
-#######################################################
-GOOGLE_API = "AIzaSyCne109pBSAhpB2rg6SlsdqIP6q5bsbp18"
+##############################################
 comuni = f"data{os.sep}comuni.json"
-gmaps = googlemaps.Client(key=GOOGLE_API, retry_over_query_limit=False)
 
 
-#######################################################
+##############################################
 
 def download_comuni(filename="comuni.json"):
     url = "https://raw.githubusercontent.com/MatteoFasulo/Algos/main/data/comuni.json"
@@ -63,75 +55,121 @@ def search_comune(comune: str):
         print(comune, geo[geo["comune"] == comune]["lat"], geo[geo["comune"] == comune]["lng"])
 
 
-def compute_weights(cities: list, filename: str, traffic_model="best_guess"):
-    night_time = datetime.datetime(2022, 1, 1, 4, 0, 0, 0)
-
-    if datetime.datetime.now() >= night_time:
-        night_time = datetime.datetime.now() + datetime.timedelta(days=2)
-
-    latitudes = []
-    longitudes = []
-    origins = []
-    destination = []
-    distances = []
-    time_needed = []
-
-    for i in range(len(cities)):
-        long, lat = search_comune(cities[i])
-        latitudes.append(lat)
-        longitudes.append(long)
-
-    weights_df = pd.DataFrame(data={"city": cities, "lat": latitudes, "long": longitudes})
-    weights_df['Distance'] = list
-    weights_df['Minutes'] = list
-
-    for i in range(len(cities)):
-        distances.clear()
-        time_needed.clear()
-        for j in range(len(cities)):
-            LatOrigin = weights_df['lat'][i]
-            LongOrigin = weights_df['long'][i]
-            origins.append((LatOrigin, LongOrigin))
-            LatDest = weights_df['lat'][j]  # Save value as lat
-            LongDest = weights_df['long'][j]  # Save value as lat
-            destination.append((LatDest, LongDest))
-            result = gmaps.distance_matrix(origins, destination, mode='driving', departure_time=night_time,
-                                           traffic_model=traffic_model)["rows"][0]["elements"][0]
-            # print(result)
-            time = round((result["duration"]["value"]) / 60)
-            result = round((result["distance"]["value"]) / 1000)
-            time_needed.append(time)
-            distances.append(result)
-            origins.clear()
-            destination.clear()
-            # print(result)
-        weights_df['Distance'][i] = distances[::]
-        weights_df['Minutes'][i] = time_needed[::]
-
-    with open(f"{filename}.json", 'w', encoding='utf-8') as file:
-        weights_df.to_json(file, force_ascii=False, indent=2)
-
-    return
+def choose_city(city_list: list):
+    city_list = {k: v for k, v in enumerate(city_list)}
+    correct = False
+    while not correct:
+        city = input("Source node (city name or ID): ").capitalize().strip()
+        try:
+            city = int(city)
+            if city in city_list.keys():
+                index = city
+                city_name = city_list[city]
+                # print(city)
+                correct = True
+        except ValueError:
+            if city in city_list.values():
+                for i in range(len(city_list)):
+                    if city_list[i] == city:
+                        index = i
+                city_name = city
+                correct = True
+            else:
+                print("\n[!] Not found\n", city_list, sep='')
+    # print(f"{index}, {city_name}")
+    return (index, city_name, city_list)
 
 
-def best_source(graph_type: bool):
-    new_df = pd.read_json("weights.json")
+def graph_italy():
+    list_cities = list_comuni()
+    index, city_name, list_cities = choose_city(list_cities)
+    source_lat, source_long = (float(search_comune(city_name)[1]), float(search_comune(city_name)[0]))
+
+    longest_path = [max(calculate_distance(source_long, source_lat, float(search_comune(list_cities[i])[0]), float(search_comune(list_cities[i])[1])) for i in range(len(list_cities)))]
+
+    m = folium.Map(location=[41.87194, 12.56738], tiles="CartoDB positron", min_zoom=5.8, max_zoom=7, zoom_start=5.8,
+                   zoom_control=True, min_lat=36, max_lat=47, min_lon=9.5, max_lon=15.5, max_bounds=True)
+
+    best_source_index, value = best_source()
+
+    """thread = threading.Thread(target=threaded_tasks, args=(list_cities,))
+    thread.start()"""
+
+    for i in range(len(list_cities)):
+        distance = calculate_distance(source_long, source_lat, float(search_comune(list_cities[i])[0]),
+                                      float(search_comune(list_cities[i])[1]))
+        if city_name == list_cities[i]:
+            folium.Circle(
+                location=(search_comune(list_cities[i])[1], search_comune(list_cities[i])[0]),
+                popup=list_cities[i],
+                radius=7500,
+                color="crimson",
+                fill=True,
+                fill_color="crimson"
+            ).add_to(m)
+        elif i == best_source_index:
+            folium.Circle(
+                location=(search_comune(list_cities[i])[1], search_comune(list_cities[i])[0]),
+                popup=f"{list_cities[i]}\n{distance}Km",
+                radius=distance * 10,
+                color="blue",
+                fill=True,
+                fill_color="blue"
+            ).add_to(m)
+        elif distance >= longest_path[0]:
+            folium.Circle(
+                location=(search_comune(list_cities[i])[1], search_comune(list_cities[i])[0]),
+                popup=f"{list_cities[i]}\n{distance}Km",
+                radius=distance * 10,
+                color="purple",
+                fill=True,
+                fill_color="purple"
+            ).add_to(m)
+        else:
+            folium.Circle(
+                location=(search_comune(list_cities[i])[1], search_comune(list_cities[i])[0]),
+                popup=f"{list_cities[i]}\n{distance}Km",
+                radius=distance * 10,
+                color="green",
+                fill=True,
+                fill_color="green"
+            ).add_to(m)
+    plugins.Fullscreen(
+        position="topright",
+        title="Fullscreen",
+        title_cancel="Exit fullscreen",
+        force_separate_button=True,
+    ).add_to(m)
+    minimap = plugins.MiniMap()
+    m.add_child(minimap)
+    if not os.path.isdir("result"):
+        os.mkdir("result")
+    m.save(os.path.join("result", "first_model.html"))
+    return os.path.abspath(f"result{os.sep}first_model.html")
+
+
+def best_source():
+    weights = list()
+    comuni = list_comuni()
+    for comune_sorgente in comuni:
+        temp = list()
+        source_lat, source_long = (float(search_comune(comune_sorgente)[1]), float(search_comune(comune_sorgente)[0]))
+        for comune_destinazione in comuni:
+            dest_lat, dest_long = (
+                float(search_comune(comune_destinazione)[1]), float(search_comune(comune_destinazione)[0]))
+            distance = calculate_distance(source_long, source_lat, dest_long, dest_lat)
+            temp.append(distance)
+        weights.append(temp)
+
+    new_df = pd.read_json("weights_1stModel.json")
     cities = new_df["city"].tolist()
     maximums = []
     best_city = None
 
-    # print(graph_type)
-    if graph_type is True:
-        # print("KM")
-        parameter = new_df["Distance"].tolist()
-    else:
-        # print("MIN")
-        parameter = new_df["Minutes"].tolist()
-
     # print((new_df["Minutes"].tolist()[43]),(new_df["Distance"].tolist()[43]), sep='\n')
 
     for i in range(len(cities)):
-        maximums.append(max(parameter[i]))
+        maximums.append(max(weights[i]))
 
     best_value = min(maximums)
     index = 0
@@ -145,153 +183,6 @@ def best_source(graph_type: bool):
     return best_city
 
 
-def choose_city(city_list: list):
-    city_list = {k: v for k, v in enumerate(city_list)}
-
-    correct = False
-    while not correct:
-        graph_type = input("Would you like to analyze distances (Km) or duration (min)? ").strip().lower()
-        if graph_type == "distances" or graph_type == "km":
-            graph_type = True
-            correct = True
-        elif graph_type == "duration" or graph_type == "min":
-            graph_type = False
-            correct = True
-    # print("choose city type path: ", graph_type)
-
-    correct = False
-    while not correct:
-        city = input("Source node (city name or ID): ").capitalize().strip()
-
-        try:
-            city = int(city)
-            if city in city_list.keys():
-                index = city
-                city_name = city_list[city]
-                correct = True
-        except ValueError:
-            if city in city_list.values():
-                for i in range(len(city_list)):
-                    if city_list[i] == city:
-                        index = i
-                city_name = city
-                correct = True
-            else:
-                print("\n[!] Not found\n", city_list, sep='')
-    return (index, city_name, graph_type)
-
-
-def threaded_tasks(list_cities: list):
-    console = Console()
-    tasks = [f"{n} ~ " for n in range(len(list_cities))]
-    with console.status("[bold green]Calculating directions...") as status:
-        i = 0
-        while tasks:
-            task = tasks.pop(0)
-            time.sleep(0.11)
-            console.log(f"{task}{list_cities[i]}")
-            i += 1
-
-
-def graph_italy(filename="weights.json"):
-    past = datetime.datetime.now()
-    list_cities = list_comuni()
-    if not os.path.isfile(filename):
-        now = datetime.datetime.now()
-        print("Starting now...")
-        compute_weights(cities=list_cities, filename="weights")
-        print(f"Ho impiegato {datetime.datetime.now() - now} per eseguire {len(list_cities)} confronti")
-
-    new_df = pd.read_json(filename)
-    cities = new_df["city"].tolist()
-    minutes = new_df["Minutes"].tolist()
-    path = new_df["Distance"].tolist()
-
-    index, city_name, graph_type = choose_city(cities)
-    best_source_index, value = best_source(graph_type)
-
-    # print(best_source_index, value)
-
-    m = folium.Map(location=[41.87194, 12.56738], tiles="CartoDB positron", min_zoom=5.8, zoom_start=5.8,
-                   zoom_control=True, min_lat=33, max_lat=50, min_lon=9.5, max_lon=15.5, max_bounds=False)
-
-    if graph_type is True:
-        parameter = path[index]
-    else:
-        parameter = minutes[index]
-
-    thread = threading.Thread(target=threaded_tasks, args=(list_cities,))
-    thread.start()
-
-    # print(decoded)
-    f1 = folium.FeatureGroup(f"{city_name}")
-
-    for i in range(len(cities)):
-        directions_result = gmaps.directions(city_name,
-                                             cities[i],
-                                             mode="driving",
-                                             units="metric",
-                                             region="IT")
-        linea = directions_result[0]
-        linea = linea.get("overview_polyline")
-        linea = linea.get("points")
-        decoded = polyline.decode(linea)  # TODO update README WITH polyline
-
-        folium.vector_layers.PolyLine(decoded, popup=f'<b>{city_name} ~ {cities[i]}</b>',
-                                      tooltip=f'{city_name} ~ {cities[i]}',
-                                      color='black', weight=1).add_to(f1)
-        if city_name == cities[i]:
-            folium.Circle(
-                location=(search_comune(cities[i])[1], search_comune(cities[i])[0]),
-                popup=f"{cities[i]}\nSource",
-                radius=7500,
-                color="crimson",
-                fill=True,
-                fill_color="crimson"
-            ).add_to(m)
-        elif parameter[i] >= max(parameter):
-            folium.Circle(
-                location=(search_comune(cities[i])[1], search_comune(cities[i])[0]),
-                popup=f"{cities[i]}\n{path[index][i]}Km\n{minutes[index][i]}min",
-                radius=parameter[i] * 10,
-                color="purple",
-                fill=True,
-                fill_color="purple"
-            ).add_to(m)
-        elif i == best_source_index:
-            folium.Circle(
-                location=(search_comune(cities[i])[1], search_comune(cities[i])[0]),
-                popup=f"{cities[i]}\n{path[index][i]}Km\n{minutes[index][i]}min",
-                radius=parameter[i] * 10,
-                color="blue",
-                fill=True,
-                fill_color="blue"
-            ).add_to(m)
-        else:
-            folium.Circle(
-                location=(search_comune(cities[i])[1], search_comune(cities[i])[0]),
-                popup=f"{cities[i]}\n{path[index][i]}Km\n{minutes[index][i]}min",
-                radius=parameter[i] * 10,
-                color="green",
-                fill=True,
-                fill_color="green"
-            ).add_to(m)
-    plugins.Fullscreen(
-        position="topright",
-        title="Fullscreen",
-        title_cancel="Exit fullscreen",
-        force_separate_button=True,
-    ).add_to(m)
-    minimap = plugins.MiniMap()
-    m.add_child(minimap)
-    f1.add_to(m)
-    folium.LayerControl().add_to(m)
-    if not os.path.isdir("result"):
-        os.mkdir("result")
-    m.save(os.path.join("result", "second_model.html"))
-    return (os.path.abspath(f"result{os.sep}second_model.html"), datetime.datetime.now() - past)
-
-
 def main():
-    path, time_needed = graph_italy()
-    return path, time_needed
+    path = graph_italy()
+    return path
