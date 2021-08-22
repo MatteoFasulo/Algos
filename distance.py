dd44dbf57de3e32b36883a81d63dad4ebeb01ef6
Matteo Fasulo <74818541+MatteoFasulo@users.noreply.github.com>
2021-08-22 10:27:30 +0200

    Add files via upload

--- distance.py
+++ distance.py
@@ -2,7 +2,7 @@ from math import sin, cos, sqrt, atan2, radians
 
 
 def calculate_distance(lon1, lat1, lon2, lat2, R=6373.0):
-    print(lon1, lat1, lon2, lat2)
+    #print(lon1, lat1, lon2, lat2)
     lat1 = radians(lat1)
     lat2 = radians(lat2)
     lon1 = radians(lon1)
